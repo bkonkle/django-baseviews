bea9efef46c86088c30edc686440650f92a17b3c
bkonkle <brandon.konkle@gmail.com>
2010-08-25 09:08:48 -0500

    Tweaked the setup script to trim the short description and include README as the long description

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
 
+try:
+    long_description = open('README.rst', 'rt').read()
+except IOError:
+    long_description = ''
+
 setup(
     name='django-baseviews',
     version='0.1',
-    description=(
-        'A small collection of base Django view classes to build upon. It is',
-        'intended to handle a lot of the repetition in common view patterns ',
-        'and allow you to focus on what makes each view different.'
-    ),
+    description='A small collection of Django view classes to build upon.',
+    long_description = long_description,
     author='Brandon Konkle',
     author_email='brandon@brandonkonkle.com',
     license='License :: OSI Approved :: BSD License',
