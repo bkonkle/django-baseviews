e83f09b618fb7d9b0d2cc5cc013c91c2904dfe8e
bkonkle <brandon.konkle@gmail.com>
2010-08-30 19:32:36 -0500

    Incremented the version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except IOError:
 
 setup(
     name='django-baseviews',
-    version='0.4',
+    version='0.4.1',
     description='A small collection of Django view classes to build upon.',
     long_description = long_description,
     author='Brandon Konkle',
