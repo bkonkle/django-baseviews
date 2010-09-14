5e364d00641d26beb7bb3ac4b48efa358f11ff15
bkonkle <brandon.konkle@gmail.com>
2010-09-14 10:38:14 -0500

    Cleaned up some pep8 violations

--- baseviews/views.py
+++ baseviews/views.py
@@ -12,26 +12,26 @@ class BasicView(object):
     cache_time = 60*5 # 5 minutes
     content_type = settings.DEFAULT_CONTENT_TYPE
     file_attachment = False
-    
+
     def __new__(cls, request, *args, **kwargs):
         instance = object.__new__(cls)
         if isinstance(instance, cls):
             instance.__init__(request, *args, **kwargs)
         return instance()
-    
+
     def __init__(self, request, *args, **kwargs):
         self.request = request
         self.args = args
         self.kwargs = kwargs
-    
+
     def __call__(self):
         """Handle the request processing workflow."""
         return self.render()
-    
+
     def get_cache_key(self):
         """Provide an opportunity to dynamically generate the cache key."""
         return self.cache_key
-    
+
     def get_context(self):
         """
         Retrieve the cached context from the cache if it exists. Otherwise,
@@ -45,66 +45,66 @@ class BasicView(object):
             if context_dict is None:
                 context_dict = self.cached_context()
                 cache.set(cache_key, context_dict, self.cache_time)
-        context_dict.update(
-            self.uncached_context()
-        )
+        context_dict.update(self.uncached_context())
         return context_dict
-    
+
     def cached_context(self):
         """Provide the context that can be cached."""
         return {}
-    
+
     def uncached_context(self):
         """Provide the context that should not be cached."""
         return {}
-    
+
     def get_template(self):
         """
         Provide an opportunity for to dynamically generate the template name.
         """
         return self.template
-    
+
     def render(self):
         """Take the context and render it using the template."""
         return render_to_response(self.get_template(), self.get_context(),
                                   RequestContext(self.request),
                                   mimetype=self.content_type)
 
+
 class AjaxView(BasicView):
     """Returns a response containing the context serialized to Json"""
     content_type = 'application/json'
-    
+
     def __call__(self):
         if not self.request.is_ajax():
             raise Http404
         return super(AjaxView, self).__call__()
-    
+
     def render(self):
         json_data = simplejson.dumps(self.get_context(),
                                      cls=DjangoJSONEncoder)
         return HttpResponse(json_data, content_type=self.content_type)
 
+
 class FormView(BasicView):
-    
+
     def __call__(self):
         self.form_options = {}
         self.form = self.get_form()
-        
+
         if self.request.method == 'POST':
             response = self.process_form()
             # If a response was returned by the process_form method, then
             # return that response instead of the standard response.
             if response:
                 return response
-        
+
         return self.render()
-    
+
     def uncached_context(self):
         """Add the form to the uncached context."""
         context = super(FormView, self).uncached_context()
         context.update({'form': self.form})
         return context
-    
+
     def process_form(self):
         """
         The method to process POST requests. Return an HttpResponse when you
@@ -113,9 +113,7 @@ class FormView(BasicView):
         """
         if self.form.is_valid():
             self.form.save()
-            return HttpResponseRedirect(
-                self.get_success_url()
-            )
+            return HttpResponseRedirect(self.get_success_url())
 
     def get_form(self):
         """
@@ -124,14 +122,13 @@ class FormView(BasicView):
         data = getattr(self.request, 'POST', None)
         if data:
             self.form_options.update({'data': data})
-        
+
         files = getattr(self.request, 'FILES', None)
         if files:
             self.form_options.update({'files': files})
-        
+
         return self.form_class(**self.form_options)
-    
+
     def get_success_url(self):
         """Get the url to redirect to upon successful form submission."""
         return self.success_url
-
